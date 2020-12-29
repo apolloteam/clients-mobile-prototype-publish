cf8d8dcdb27dc6109b58601efa7ac6783133d0e4
ehsomma <ehsomma@gmail.com>
2020-12-29 07:32:38 -0300

    Index.

---
+++ Index_files/resources/story.js
@@ -0,0 +1,239 @@
+var story = {
+ "title": "Index",
+ "pages": [
+  {
+   "title": "Booking",
+   "height": 2533,
+   "width": 1341,
+   "image": "Booking.png",
+   "links": [{
+    "page": 3,
+    "rect": [
+     34,
+     1464,
+     374,
+     1500
+    ]
+   }],
+   "image2x": "Booking@2x.png"
+  },
+  {
+   "title": "Booking-carfeatures",
+   "height": 794,
+   "width": 406,
+   "image": "Booking-carfeatures.png",
+   "links": [{
+    "page": 5,
+    "rect": [
+     33,
+     615,
+     373,
+     651
+    ]
+   }],
+   "image2x": "Booking-carfeatures@2x.png"
+  },
+  {
+   "title": "Booking-carfeatures2",
+   "height": 799,
+   "width": 1421,
+   "image": "Booking-carfeatures2.png",
+   "links": [
+    {
+     "page": 5,
+     "rect": [
+      49,
+      615,
+      389,
+      651
+     ]
+    },
+    {
+     "page": 5,
+     "rect": [
+      585,
+      615,
+      925,
+      651
+     ]
+    },
+    {
+     "page": 4,
+     "rect": [
+      1048,
+      620,
+      1388,
+      656
+     ]
+    }
+   ],
+   "image2x": "Booking-carfeatures2@2x.png"
+  },
+  {
+   "title": "Booking-carselect",
+   "height": 794,
+   "width": 696,
+   "image": "Booking-carselect.png",
+   "links": [{
+    "page": 1,
+    "rect": [
+     278,
+     416,
+     368,
+     455
+    ]
+   }],
+   "image2x": "Booking-carselect@2x.png"
+  },
+  {
+   "title": "Booking-confirmation",
+   "height": 806,
+   "width": 1355,
+   "image": "Booking-confirmation.png",
+   "links": [{
+    "page": 10,
+    "rect": [
+     33,
+     616,
+     373,
+     652
+    ]
+   }],
+   "image2x": "Booking-confirmation@2x.png"
+  },
+  {
+   "title": "Booking-costCenter",
+   "height": 794,
+   "width": 406,
+   "image": "Booking-costCenter.png",
+   "links": [{
+    "page": 4,
+    "rect": [
+     33,
+     615,
+     373,
+     651
+    ]
+   }],
+   "image2x": "Booking-costCenter@2x.png"
+  },
+  {
+   "title": "Booking-flight",
+   "height": 805,
+   "width": 1001,
+   "image": "Booking-flight.png",
+   "links": [{
+    "page": 4,
+    "rect": [
+     33,
+     615,
+     373,
+     651
+    ]
+   }],
+   "image2x": "Booking-flight@2x.png"
+  },
+  {
+   "title": "Booking-ondemand",
+   "height": 1805,
+   "width": 1227,
+   "image": "Booking-ondemand.png",
+   "links": [],
+   "image2x": "Booking-ondemand@2x.png"
+  },
+  {
+   "title": "ForceChangeUserName",
+   "height": 794,
+   "width": 406,
+   "image": "ForceChangeUserName.png",
+   "links": [],
+   "image2x": "ForceChangeUserName@2x.png"
+  },
+  {
+   "title": "Login",
+   "height": 1488,
+   "width": 836,
+   "image": "Login.png",
+   "links": [{
+    "page": 10,
+    "rect": [
+     714,
+     317,
+     826,
+     353
+    ]
+   }],
+   "image2x": "Login@2x.png"
+  },
+  {
+   "title": "Main",
+   "height": 1632,
+   "width": 980,
+   "image": "Main.png",
+   "links": [
+    {
+     "page": 0,
+     "rect": [
+      358,
+      584,
+      416,
+      642
+     ]
+    },
+    {
+     "page": 0,
+     "rect": [
+      854,
+      584,
+      912,
+      642
+     ]
+    },
+    {
+     "page": 0,
+     "rect": [
+      854,
+      1424,
+      912,
+      1482
+     ]
+    }
+   ],
+   "image2x": "Main@2x.png"
+  },
+  {
+   "title": "SeatReservation",
+   "height": 2577,
+   "width": 1054,
+   "image": "SeatReservation.png",
+   "links": [],
+   "image2x": "SeatReservation@2x.png"
+  },
+  {
+   "title": "Transfer",
+   "height": 1619,
+   "width": 900,
+   "image": "Transfer.png",
+   "links": [],
+   "image2x": "Transfer@2x.png"
+  },
+  {
+   "title": "Welcome",
+   "height": 794,
+   "width": 705,
+   "image": "Welcome.png",
+   "links": [{
+    "page": 9,
+    "rect": [
+     160,
+     616,
+     248,
+     652
+    ]
+   }],
+   "image2x": "Welcome@2x.png"
+  }
+ ],
+ "highlightLinks": true,
+ "resolutions": [2]
+}
